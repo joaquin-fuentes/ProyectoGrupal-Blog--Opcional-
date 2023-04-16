57a8914d36abcc49d467469c09b3aadfe98fe56b
leandro <leandro_navarro@outlook.es>
2023-04-16 17:23:34 -0300

    funcion de publicidad creada y testeada

---
+++ js/publicidad.js
@@ -0,0 +1,9 @@
+let propaganda = document.getElementById("propaganda");
+
+setTimeout(() => {
+  propaganda.classList.remove("desaparece");
+  propaganda.classList.add("aparece");
+}, 4000);
+
+
+//Ver de arreglar esto para que funcione igual en computadoras con add block
